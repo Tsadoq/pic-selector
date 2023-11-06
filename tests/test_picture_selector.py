db93b44320e91e0660682ad96dfc727cfdf5dd7e
Tsadoq <github.v5331@passmail.net>
2023-11-06 08:52:03 +0100

    badge

--- tests/test_picture_selector.py
+++ tests/test_picture_selector.py
@@ -10,8 +10,8 @@ def test_picture_selector():
 
 def test_picture_selector_read_files():
     at = AppTest.from_file("../picture_selector.py")
-    at.session_state.root_path = os.path.abspath('./resources/root_test')
-    at.session_state.dest_path = os.path.abspath('./resources/dest_test')
+    at.session_state.root_path = './resources/root_test'
+    at.session_state.dest_path = './resources/dest_test'
     at.run()
     at.button(key='next').click()
     at.run()
